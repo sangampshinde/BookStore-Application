2bcc5bd028017ba96a176f7058367fcf5f7f11a5
sangam shinde <sangampshinde@gmail.com>
2024-07-25 22:49:37 +0530

    conneting database

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
+
+// db connection
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://admin:admin%401@cluster0.pdbgysg.mongodb.net/AuthDB"
+    );
+    console.log("Connected to Database");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.listen(8800, () => {
+  connectMongoDB();
+  console.log("Server is running on port 8800");
+});
