2c0f078da81c6bb8435fbfae7a1ea2198d6b75cb
sangam shinde <sangampshinde@gmail.com>
2024-07-25 23:07:12 +0530

    db connect standerd way

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
+
 
 // db connection
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://admin:admin%401@cluster0.pdbgysg.mongodb.net/AuthDB"
-    );
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Connected to Database");
   } catch (error) {
     throw error;
