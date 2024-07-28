2cb9258486b25ce6610d025f2a4dc087e592a13e
sangam shinde <sangampshinde@gmail.com>
2024-07-28 21:16:21 +0530

    role route create

--- backend/routes/roleRoute.js
+++ backend/routes/roleRoute.js
@@ -1,44 +1,18 @@
 import express from "express";
-import Role from "../models/Role.js";
+import { createRole, deleteRole, getAllRoles, updateRole } from "../controllers/role.controller.js";
 
 const router = express.Router();
 
 // Create a new Role in the database
-router.post("/create", async (req, res, next) => {
-  try {
-    if (req.body.role && req.body.role !== "") {
-      const newRole = new Role(req.body);
-      await newRole.save();
-      return res.send("Role Created");
-    } else {
-      return res.status(400).send("Bad Request");
-    }
-  } catch (error) {
-    return res.status(500).send("Internal Server Error");
-  }
-});
+router.post("/create",createRole );
 
 // Upadte Role in database
-router.put("/update/:id",async(req,res,next) => {
-  try {
+router.put("/update/:id",updateRole);
 
-    const role = await Role.findById({_id: req.params.id});
-    if(role){
-      const newData = await Role.findByIdAndUpdate(
-        req.params.id,
-        {$set: req.body},
-        {new: true}
-      );
-      res.status(200).send("Role updated");
-    }else{
-      res.status(404).send("Role not found!");
-    }
+// Get All Roles from the database
+router.get("/getAll",getAllRoles);
 
-    
-  } catch (error) {
-    return res.status(500).send("Internal Server Error");
-  }
-
-})
+// delete role from database
+router.delete("/delete/:id",deleteRole);
 
 export default router;
