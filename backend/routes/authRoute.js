0069baa3059abffbc85901a5a3c21d6fd56cc7e2
sangam shinde <sangampshinde@gmail.com>
2024-07-30 21:46:22 +0530

    login and password incrypt

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register } from '../controllers/auth.controller.js';
+import { login, register } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ const router = express.Router();
  router.post("/register",register);
 
 //  login routes
+router.post("/login",login);
 
 
 
